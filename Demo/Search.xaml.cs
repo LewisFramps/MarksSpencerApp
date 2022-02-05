c7c6dae350501e06b26ede6b3a0e5a96be2eafb8
hafniz <42380178+hafniz@users.noreply.github.com>
2022-02-05 22:07:31 +0000

    Search result page

--- Demo/Search.xaml.cs
+++ Demo/Search.xaml.cs
@@ -19,6 +19,8 @@ namespace MnSDemo;
 /// </summary>
 public partial class Search : Page
 {
+    string searchTermRaw, searchTermLower;
+
     public Search()
     {
         InitializeComponent();
@@ -27,6 +29,37 @@ public partial class Search : Page
 
     private void Page_Loaded(object sender, RoutedEventArgs e)
     {
-        TitleBlock.Text = $"Search results: {NavigationService.CurrentSource.ToString().Split("=")[1]}";
+        searchTermRaw = NavigationService.CurrentSource.ToString().Split("=")[1];
+        searchTermLower = searchTermRaw.ToLower();
+        TitleBlock.Text = $"Search results: {searchTermRaw}";
+        QuantityBlock.Text = $"{(searchTermLower == "towel" ? "1,919" : "114,514")} items found";
+
+        string capitalized = $"{char.ToUpper(searchTermRaw[0])}{searchTermLower[1..]}";
+        Title1.Text = capitalized + '1';
+        Title2.Text = capitalized + '2';
+        Title3.Text = capitalized + '3';
+        Title4.Text = capitalized + '4';
+        Image1.Source = (ImageSource)new ImageSourceConverter().ConvertFromString($"../../../assets/{searchTermLower}1.jpg");
+        Image2.Source = (ImageSource)new ImageSourceConverter().ConvertFromString($"../../../assets/{searchTermLower}2.jpg");
+        Image3.Source = (ImageSource)new ImageSourceConverter().ConvertFromString($"../../../assets/{searchTermLower}3.jpg");
+        Image4.Source = (ImageSource)new ImageSourceConverter().ConvertFromString($"../../../assets/{searchTermLower}4.jpg");
     }
+
+    private void StackPanel3_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (searchTermLower == "towel")
+        {
+            NavigationService.Navigate(new Uri($"Product.xaml?id=3", UriKind.Relative));
+        }
+    }
+
+    private void StackPane2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (searchTermLower == "candle")
+        {
+            NavigationService.Navigate(new Uri($"Product.xaml?id=4", UriKind.Relative));
+        }
+    }
+
+
 }
